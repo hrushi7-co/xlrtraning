aab57fc0069d5e05aa65cb5f70139fa628c777ec
hrushi7-co <saihrushikeshsai@gmail.com>
2025-02-05 11:16:11 +0530

    Add files via upload

---
+++ PrintArrayIndices.java
@@ -0,0 +1,12 @@
+public class PrintArrayIndices {
+    public static void main(String[] args) {
+        // Define an array
+        int[] arr = {10, 20, 30, 40, 50};
+
+        // Loop through the array and print the index values
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println("Index " + i + ": " + arr[i]);
+        }
+    }
+}
+    
